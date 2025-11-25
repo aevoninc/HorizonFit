c8a1872aaffc1566614fbeac5090894479b48c0e
gurumaniakash1510-cyber <gurumaniakash1510@gmail.com>
2025-11-25 18:17:58 +0530

    Backend changes before merging

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,25 @@
-// import {app} from "./app.js";
-// import { configDotenv } from "dotenv";
-// import connectdb from "./db/db.js";
-
-// configDotenv({path:"./.env"});
-
-// await connectdb();
-
-// app.listen(process.env.PORT || 8000, () => {
-//     console.log("Server is running on port 3000");
-// })
-import { app } from "./app.js";
-import "dotenv/config";
+import {app} from "./app.js";
+import { configDotenv } from "dotenv";
 import connectdb from "./db/db.js";
 
 configDotenv({path:"./.env"});
 
 await connectdb();
 
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Server is running on port 3000");
+})
+// import { app } from "./app.js";
+// import "dotenv/config";
+// import connectdb from "./db/db.js";
 
-const PORT = process.env.PORT || 8000;
+// configDotenv({path:"./.env"});
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// await connectdb();
+
+
+// const PORT = process.env.PORT || 8000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
